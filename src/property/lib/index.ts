4e8d43dd3deea609d31931128c5f22927f325ad2
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-08-17 00:12:59 +0000

    refactor(property): remove.

--- src/property/lib/index.ts
+++
@@ -1,3 +0,0 @@
-export { PropertyControllerCore } from './property-controller-core.abstract ';
-export { PropertyControllerBase } from './property-controller-base.abstract';
-export { PropertyController } from './property-controller.class';
